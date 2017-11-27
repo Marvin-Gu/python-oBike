8d2280cd1314f2de4b2dfeb07d4a5b53bdab1937
Marvin-Gu <960292946@qq.com>
2017-11-27 19:57:52 +0800

    输入点击刷新删除输入退出

---
+++ baiduquitclear.py
@@ -0,0 +1,30 @@
+# coding = utf-8
+
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="./chromedriver")
+
+driver.get("http://www.baidu.com")
+
+driver.find_element_by_id("kw").send_keys("selenium")
+
+driver.find_element_by_id("su").click()
+
+from time import sleep
+
+sleep(5)
+
+driver.refresh()
+
+
+driver.find_element_by_id("kw").clear()
+
+driver.find_element_by_id("kw").send_keys("什么时候下班")
+
+driver.find_element_by_id("su").click()
+
+from time import sleep
+
+sleep(5)
+
+driver.quit()
