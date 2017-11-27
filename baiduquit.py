bb8f438c0c508257c18cadf93a4c02a13d9cfd01
Marvin-Gu <960292946@qq.com>
2017-11-27 16:24:50 +0800

    百度搜索退出

---
+++ baiduquit.py
@@ -0,0 +1,13 @@
+# coding = utf-8
+
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="./chromedriver")
+
+driver.get("http://www.baidu.com")
+
+driver.find_element_by_id("kw").send_keys("selenium")
+
+driver.find_element_by_id("su").click()
+
+driver.quit()
