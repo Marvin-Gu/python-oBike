67c4562d4f8bad18c4c3c3b967aa2b23c92571ad
Marvin-Gu <960292946@qq.com>
2017-11-28 10:48:17 +0800

    clear

--- baiduquitclear.py
+++ baiduquitclear.py
@@ -23,6 +23,13 @@ driver.find_element_by_id("kw").send_keys("什么时候下班")
 
 driver.find_element_by_id("su").click()
 
+driver.find_element_by_id("kw").clear()
+
+driver.find_element_by_id("kw").send_keys("你在做梦吧")
+
+driver.find_element_by_id("su").click()
+
+
 from time import sleep
 
 sleep(5)
